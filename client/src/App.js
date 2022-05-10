88565377a3b7210f6bee2969f33d818046c11389
savage-sean91 <Savage.Sean91@gmail.com>
2022-05-09 21:23:45 -0400

    just a push
    push 05/01/2022

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <ThemeProvider>
-      <div className="App">
+      < div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {testData ? (
